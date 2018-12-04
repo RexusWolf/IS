6eef9f250d50c528568f741eda0c9b2e14e59a3d
Antonio Muñoz Cubero <tonicubero12@gmail.com>
2018-12-04 10:28:37 +0100

    added seach student code

---
+++ Implementation/id_001/id_001.cc
@@ -0,0 +1,88 @@
+// Search Student function
+//The function let you search an student in the list.
+//The function reutrn the number of student that we found with a surmano o dni
+
+
+/* primero abro database y compruebo de primera mano cuantos alumnos existen
+con el apellido introducido, si no hay ninguno, se retorna 0, si existe mas de
+uno, pido el dni para acotar la busqueda.
+*/
+
+#include "persona.h"
+#include "teacher.h"
+#include "student.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+std::string aux_surname;
+int counter = 0;
+
+int Find_Student(){
+  //We use a txt file as database, so, let's open .txt
+  //Declare an input file stream (ifstream) variable.
+
+  ifstream inFile;
+
+  inFile.open("database.txt");
+  //check if the file exist or can be open.
+  if (!inFile){
+    std::cout << "Not able to open database" << \n;
+    //finish the program with an error.
+    exit(1);
+  }
+
+  while (inFile >> aux_surname) {
+    if (aux_surname == student.surname) {
+      /* if the student exist, increase the counter to know how many Student
+      exist with the same surname */
+      counter++;
+    }
+  }
+
+  //if counter is 0, is because there aren't student whith this surname
+  if (counter == 0){
+    std::cout << "Student not found" << '\n';
+    return counter
+  }
+
+  //if counter is 1, only found 1 sudent so, return 1 "the numer of Student with this surname"
+  if (counter == 1){
+    std::cout << "Student found" << '\n';
+    return counter;
+  }
+
+  //if we get more than 1 matches, we have to search by dni
+  if (counter > 1){
+    std::cout << "More than 1 student found, start searching by dni" << '\n';
+    std::cout << "Instert the dni of the student." << '\n';
+    std::cin >> aux_surname;
+    //the same algoritmo de antes xd
+    counter = 0
+    while (inFile >> aux_surname){
+      if (aux_surname == student.dni){
+        counter++
+      }
+    }
+
+    //if counter is 0, is because there aren't student whith this surname
+    if (counter == 0){
+      std::cout << "Student not found" << '\n';
+      return counter
+    }
+
+    //if counter is 1, only found 1 sudent so, return 1 "the numer of Student with this surname"
+    if (counter == 1){
+      std::cout << "Student found" << '\n';
+      return counter;
+    }
+
+    if (counter > 2){
+      std::cout << "Error" << '\n';
+      exit(0);
+    }
+  }
+
+
+inFile.close();
+}
