b7ca3301dabfdb4df2f341debe8111305881a562
RexusWolf <francisjmp99@gmail.com>
2018-12-16 14:54:51 +0100

    id_005 done. (Needs revision)

---
+++ Implementation/id_005/id_005.cc
@@ -0,0 +1,17 @@
+#include "student.h"
+#include <string>
+#include <list>
+#include <iostream>
+#include "id_005.h"
+using std::list;
+using std::string
+
+void showStudents(list<Student> students){
+  list<Student>iterator it = students.begin();
+  list<Student>iterator e = students.end();
+  string namefile = "Students.md"
+  std::ofstream file(namefile); // Outfile creation.
+  for(it; it != e; it++){ // Displacement of the iterator through the list.
+    file << *it << "\n";
+  }
+}
---
+++ Implementation/id_005/id_005.h
@@ -0,0 +1,6 @@
+#ifndef ID_005_H
+#define
+
+  void showStudents(list<Student> students);
+
+#endif
